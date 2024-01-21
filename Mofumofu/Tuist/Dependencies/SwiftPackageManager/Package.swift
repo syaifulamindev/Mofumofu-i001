56540fe4af2835f73dd77652899f921381c5a27b
Syaiful Amin <sayfullamin@gmail.com>
2024-01-21 15:39:02 +0700

    fix git ignore

---
+++ Mofumofu/Tuist/Dependencies/SwiftPackageManager/Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+
+let package = Package(
+    name: "",
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.8.1"),
+        .package(url: "https://github.com/Moya/Moya.git", from: "15.0.0"),
+        .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.0.1"),
+        
+    ]
+)
+
+
